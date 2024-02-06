0863d4d209a5be61945aad38132be7375c580545
sumit <sumitkumar.chaturvedi@adevinta.com>
2024-02-06 03:39:41 +0100

    test

--- .github/scripts/generate-release-notes.py
+++ .github/scripts/generate-release-notes.py
@@ -205,7 +205,6 @@ def create_draft_release(repo, release_notes, version):
     latest_release.update_release(
         name=latest_release.title,
         message=formatted_message,
-        tag=version,
         draft=True
     )
 
