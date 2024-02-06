4324e3c82b16b2e4a2d49f5c278025df136b2be6
chaturvedisumit <48179444+chaturvedisumit@users.noreply.github.com>
2024-02-06 01:21:40 +0100

    Update generate-release-notes.py

--- .github/scripts/generate-release-notes.py
+++ .github/scripts/generate-release-notes.py
@@ -194,7 +194,7 @@ def create_draft_release(repo, release_notes, version):
     release_notes_merged = release.body + '\n\n' + release_notes
 
     message = group_release_info(release_notes_merged)
-    print("message")
+    print("************************message*******************************", message)
 
     # Upload release notes
     release.update_release(
